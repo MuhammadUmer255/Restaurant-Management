599e08933306d07f0395c3e19bb2d6cad0c5af32
Muhammad Umer <muhammad.umer25052@gmail.com>
2026-09-16 21:00:57 +0500

    fix issue

--- App.js
+++ App.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
+import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
   return (
-    <View style={styles.container}>
+    <>
       <StatusBar barStyle="light-content" backgroundColor="#070E20" />
-      <Text style={styles.title}>GourmetOS POS</Text>
-      <Text style={styles.subtitle}>Terminal Connected Successfully!</Text>
-    </View>
+      <AppNavigator />
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#070E20',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    color: '#ff7a1a',
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  subtitle: {
-    color: '#ffffff',
-    fontSize: 14,
-    marginTop: 8,
-  },
-});
 
 export default App;
